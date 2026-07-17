4775ddae247f017804402da18eb48227dffe777e
ruslanrusselpython <ruslan19041994@gmail.com>
2026-07-17 12:45:51 +0300

    Fix: escape $ in embedded JSON to prevent JS replace backreference corruption

--- embed.js
+++ embed.js
@@ -7,9 +7,10 @@ const json = fs.readFileSync(path.join(dir, 'data.json'), 'utf8');
 let html = fs.readFileSync(path.join(dir, 'index.html'), 'utf8');
 
 const escaped = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+// Use function to avoid $ backreference issues in replacement string
 html = html.replace(
   /(<script id="embedded-data" type="application\/json">)[\s\S]*?(<\/script>)/,
-  '$1' + escaped + '$2'
+  function(match, open, close) { return open + escaped + close; }
 );
 
 fs.writeFileSync(path.join(dir, 'index.html'), html, 'utf8');
